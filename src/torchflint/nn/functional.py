839ea8591706b158bfac1743ee12983f0f550f82
J. Zhang <79358184+Caewinix@users.noreply.github.com>
2025-12-14 17:33:34 +0800

    Delete src/torchflint directory

--- src/torchflint/nn/functional.py
+++
@@ -1,9 +0,0 @@
-from torch.nn import Module, DataParallel
-from torch.nn.parallel import DistributedDataParallel
-
-
-def refine_model(model: Module):
-    if isinstance(model, DataParallel) or isinstance(model, DistributedDataParallel):
-        return model.module
-    else:
-        return model
