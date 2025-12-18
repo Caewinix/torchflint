feeed2fa1b0ec5ec4e17ca46f99759397f5316ec
J. Zhang <79358184+Caewinix@users.noreply.github.com>
2025-12-18 17:36:46 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'TorchFlint'
 copyright = '2025, Caewinix'
 author = 'Caewinix'
-release = '0.0.1b16'
+release = '0.0.1b16.post1.dev1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
