6188cdee651bf26a35f7081f64a7a1c5a0d051f4
J. Zhang <79358184+Caewinix@users.noreply.github.com>
2026-01-09 00:37:25 +0800

    Delete src/torchflint directory

--- src/torchflint/utils/_helper.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-
-def __module__(name: str) -> Any:
-    def module_registrant(attribute: Any):
-        attribute.__module__ = name
-        return attribute
-    return module_registrant
